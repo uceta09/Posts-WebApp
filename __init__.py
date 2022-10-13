57962f36d8bccea9c32454d6601414e3c269bfc8
Juan Elvin Uceta <102885414+uceta09@users.noreply.github.com>
2022-10-13 18:27:46 -0400

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,32 @@
+import os
+from flask import Flask
+from . import db
+from . import auth
+from . import blog
+
+def create_app(test_config=None):
+    #create and configure the web app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+    
+    db.init_app(app)
+    app.register_blueprint(auth.bp)
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+    
+    if test_config is None:
+        #load the instance config, if it exist, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        #load the test config if passed in
+        app.config.from_mapping(test_config)
+        
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+        
+    return app
